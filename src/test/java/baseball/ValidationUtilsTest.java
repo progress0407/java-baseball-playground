9e8106d1625a713c82b87cafc9b2025f2c48880c
swcho <progress0407@gmail.com>
2021-11-13 13:00:48 +0900

    feat: 숫자 검증 코드
    검증 코드 구현
    - ValidationUtils.java 작성
    - ValidationUtilsTest.java 테스트코드

---
+++ src/test/java/baseball/ValidationUtilsTest.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ValidationUtilsTest {
+
+    /**
+     * 경곗값을 중심으로 검증 코드를 짠다
+     */
+
+    @Test
+    @DisplayName("야구 숫자 1 ~ 9 검증")
+    void 야구_숫자_1_9_검증() {
+        assertThat(ValidationUtils.valid(0)).isFalse();
+        assertThat(ValidationUtils.valid(1)).isTrue();
+        assertThat(ValidationUtils.valid(9)).isTrue();
+        assertThat(ValidationUtils.valid(10)).isFalse();
+    }
+}
